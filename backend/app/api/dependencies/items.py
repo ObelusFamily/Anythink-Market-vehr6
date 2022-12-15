94250f22fd364d5abc2cdc12ebd9fc313799f273
Ken <40008994+kenkaw@users.noreply.github.com>
2022-12-16 12:22:09 +1300

    Update items.py

--- backend/app/api/dependencies/items.py
+++ backend/app/api/dependencies/items.py
@@ -19,20 +19,20 @@ from app.services.items import check_user_can_modify_item
 
 
 def get_items_filters(
-    title: Optional[str] = None,
     tag: Optional[str] = None,
     seller: Optional[str] = None,
     favorited: Optional[str] = None,
     limit: int = Query(DEFAULT_ITEMS_LIMIT, ge=1),
-    offset: int = Query(DEFAULT_ITEMS_OFFSET, ge=0)
+    offset: int = Query(DEFAULT_ITEMS_OFFSET, ge=0),
+    title: Optional[str] = None
 ) -> ItemsFilters:
     return ItemsFilters(
-        title=title,
         tag=tag,
         seller=seller,
         favorited=favorited,
         limit=limit,
-        offset=offset
+        offset=offset,
+        title=title
     )
 
 
